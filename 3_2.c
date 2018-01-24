50b915ce176bf612cf05170c8f7605bcf25c3125
szaraki <34246855+szaraki@users.noreply.github.com>
2018-01-24 14:04:05 +0100

    Update 3_2.c

--- 3_2.c
+++ 3_2.c
@@ -1,31 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-#define True 1
-#define False 0
 
 
-
-double f2c(double fahrenheit) {
+int fahrenheit2celsius(double fahrenheit) {
 	const double f2c_static =5./9.;
 	return (fahrenheit-32.)*f2c_static;
 }
 
-
-double c2f(double celsius) {
+int celsius2fahrenheit(double celsius) {
 	const double c2f_static =9./5.;
 	return celsius*c2f_static + 32.;
 }
 
-
-
-
 int main(){
-	
-	
-	
-	
+	printf("%d\n", fahrenheit2celsius(100));
+	printf("%d\n", celsius2fahrenheit(100));
 	
 	return 0;
 }
