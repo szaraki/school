8998e7ab4e75883c28778d9f89c932fa7859ad9e
szaraki <34246855+szaraki@users.noreply.github.com>
2018-01-24 14:06:31 +0100

    Update 3_3.c

--- 3_3.c
+++ 3_3.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 #define PI 3.14
 
 
-
 double rad2degrees(double rad) {
-	
 	return rad* 180./PI;
-	
 }
 
 double degrees2rad(double degrees) {
-	
 	return degrees*PI/180.;
 }
 
-
 int main(){
-	
-	
-	
-	
+	printf("%lf\n", rad2degrees(100));
+	printf("%lf\n", degrees2rad(100));
 	
 	return 0;
 }
